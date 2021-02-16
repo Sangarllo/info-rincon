be32936b465f42eb8f6ea78ccfa36dd819b7b3ea
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2021-02-16 19:15:04 +0100

    Fix quotes problem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   baseUrl: 'https://info-rincon.web.app/',
   firebase: {
-    apiKey: "AIzaSyChFHMB9Kj4sEBdPKLRWu-JFVMg7gketuM",
-    authDomain: "info-rincon.firebaseapp.com",
-    projectId: "info-rincon",
-    storageBucket: "info-rincon.appspot.com",
-    measurementId: "G-46CK1QBGRK",
-    messagingSenderId: "108949919658",
-    appId: "1:108949919658:web:f6b2983174d46837cafdd3",
+    apiKey: 'AIzaSyChFHMB9Kj4sEBdPKLRWu-JFVMg7gketuM',
+    authDomain: 'info-rincon.firebaseapp.com',
+    projectId: 'info-rincon',
+    storageBucket: 'info-rincon.appspot.com',
+    measurementId: 'G-46CK1QBGRK',
+    messagingSenderId: '108949919658',
+    appId: '1:108949919658:web:f6b2983174d46837cafdd3',
   }
 };
