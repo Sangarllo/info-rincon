4ce4cc77d7e0b1777b29b235546e1d46723c468d
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2021-06-14 20:26:03 +0200

    Style calendar headers

--- src/app/pages/calendar/calendar-header.component.ts
+++ src/app/pages/calendar/calendar-header.component.ts
@@ -3,7 +3,8 @@ import { CalendarView } from 'angular-calendar';
 
 @Component({
   selector: 'app-calendar-header',
-  templateUrl: './calendar-header.component.html'
+  templateUrl: './calendar-header.component.html',
+  styleUrls: ['./calendar-header.component.scss'],
 })
 export class CalendarHeaderComponent {
   @Input() view: CalendarView;
