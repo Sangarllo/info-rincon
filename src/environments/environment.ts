128bdb0c073e0aeadebb069e1120f25613520da2
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2022-06-23 00:11:32 +0200

    Fix name of variable for claps and favorites.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  appVersion: '2.0.2.049',
-  releaseDate: '2022-06-20 00:00',
+  appVersion: '2.0.2.050',
+  releaseDate: '2022-06-23 00:00',
   baseUrl: 'https://agendarinconera.es',
   // baseUrl: 'http://localhost:4200',
   setAudit: true,
